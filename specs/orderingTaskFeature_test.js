10755a2b07b5293336eade8f9343d6398b7978f2
Eric Carles <carles.eric@wanadoo.fr>
2018-02-16 17:12:47 +0000

    OrderingTaskFeature class : get 2 kinds of order features passed

--- specs/orderingTaskFeature_test.js
+++ specs/orderingTaskFeature_test.js
@@ -2,20 +2,20 @@ const assert = require("assert");
 const OrderingTaskFeature = require("../OrderingTaskFeature");
 
 
-describe("OrderingTaskSens test", function(){
+describe("OrderingTaskFeature test", function(){
 
-  let orderingTaskSens;
+  let orderingTaskFeature;
 
   beforeEach("Setup", function(){
-    orderingTaskSens = new OrderingTaskFeature();
+    orderingTaskFeature = new OrderingTaskFeature();
   })
 
   it("should have an ascending order", function(){
-    assert.strictEqual("ascending", orderingTaskSens.ascending);
+    assert.strictEqual("ascending", orderingTaskFeature.ascending);
   });
 
   it("should have an descending order", function(){
-    assert.strictEqual("descending", orderingTaskSens.descending);
+    assert.strictEqual("descending", orderingTaskFeature.descending);
   });
 
 })
