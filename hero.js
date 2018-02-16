e90d7f81c1491a8e7a2c6ab811fbb5406501e40c
Eric Carles <carles.eric@wanadoo.fr>
2018-02-16 14:22:52 +0000

    Hero class : method getHealth() passed

--- hero.js
+++ hero.js
@@ -1,11 +1,16 @@
-const Hero = function (name, favouriteFood){
+const Hero = function (name, health, favouriteFood){
   this.name          = name;
+  this.health        = health;
   this.favouriteFood = favouriteFood;
 }
 
 
 Hero.prototype.getName = function(){
   return this.name;
+}
+
+Hero.prototype.getHealth = function(){
+  return this.health;
 }
 
 Hero.prototype.getFavouriteFood = function(){
--- specs/hero_test.js
+++ specs/hero_test.js
@@ -8,12 +8,16 @@ const Hero   = require('../hero.js');
     let hero;
 
     beforeEach("Setup", function(){
-      hero = new Hero("SpongeBob", "plankton");
+      hero = new Hero("SpongeBob", 20, "plankton");
     })
 
 
     it("should have a name", function(){
       assert.strictEqual("SpongeBob", hero.getName());
+    })
+
+    it("should have a health points counter", function(){
+        assert.strictEqual(20, hero.getHealth());
     })
 
     it("should have a favourite food", function(){
